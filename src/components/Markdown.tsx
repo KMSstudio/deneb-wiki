1eafad31d8b0c84659cdb9b5a3afe7674990b7cf
Kang Myeong Seok <tomskang@naver.com>
2025-10-03 18:17:17 +0900

    Add code highlighter to component/markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -7,9 +7,12 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
+import rehypeHighlight from "rehype-highlight";
 
 import "katex/dist/katex.min.css";
 import "github-markdown-css/github-markdown.css";
+import "highlight.js/styles/github-dark.css"; // 테마 (github, atom-one-dark, vs 등 가능)
+import styles from "./markdown.module.css";
 
 type MarkdownProps = {
   content: string;
@@ -18,12 +21,12 @@ type MarkdownProps = {
 
 export default function Markdown({ content, className }: MarkdownProps) {
   return (
-    <div className={className ?? "markdown-body"}>
+    <div className={`${className ?? "markdown-body"} ${styles.markdown}`}>
       <ReactMarkdown
         // GFM + LaTeX
         remarkPlugins={[remarkGfm, remarkMath]}
         // raw HTML allow + ban XSS + KaTeX render
-        rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeKatex]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeKatex, rehypeHighlight]}
       >
         {content}
       </ReactMarkdown>
